faf58aec819483f5707a9f66453e9409f914223c
vguttmann <vincent.guttmann@gmx.de>
2025-01-04 15:59:07 +0100

    Added StateError

--- src/roomba.py
+++ src/roomba.py
@@ -18,9 +18,21 @@ class InputError(Error):
         `message`:
             explanation of the error
     """
-
     def __init__(self, expression, message):
         self.expression = expression
+        self.message = message
+
+class StateError(Error):
+    """Exception raised for errors in the input.
+
+    Attributes:
+        `expression`:
+            input expression in which the error occurred
+        
+        `message`:
+            explanation of the error
+    """
+    def __init__(self, message):
         self.message = message
 
 class Roomba:
